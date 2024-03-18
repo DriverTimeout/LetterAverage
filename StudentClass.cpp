ee4a6bb220f3a608476e695bcf589e606e87f515
DriverTimeout <128652615+DriverTimeout@users.noreply.github.com>
2024-03-18 13:30:22 -0500

    Update StudentClass.cpp

--- StudentClass.cpp
+++ StudentClass.cpp
@@ -3,17 +3,56 @@
 StudentClass::StudentClass(string n) : Name(n) {}
 
 string StudentClass::Student() {
-    return Name;
+	return Name;
 }
 
 void StudentClass::AddAverage(double s) {
-    FinGrades += s;
+	FinGrades += s;
 }
 
 double StudentClass::CalAverage(double amount) {
-    return FinGrades / amount;
+	return FinGrades / amount;
 }
 
-string StudentClass::GetLetterAverage(string Name) {
-    return Name;
+string StudentClass::GetLetterAverage(double average) {
+	if (average >= 100) {
+		return "A+";
+	}
+	else if (average >= 93) {
+		return "A";
+	}
+	else if (average >= 90) {
+		return "A-";
+	}
+	else if (average >= 87) {
+		return "B+";
+	}
+	else if (average >= 83) {
+		return "B";
+	}
+	else if (average >= 80) {
+		return "B-";
+	}
+	else if (average >= 77) {
+		return "C+";
+	}
+	else if (average >= 73) {
+		return "C";
+	}
+	else if (average >= 70) {
+		return "C-";
+	}
+	else if (average >= 67) {
+		return "D+";
+	}
+	else if (average >= 63) {
+		return "D";
+	}
+	else if (average >= 60) {
+		return "D-";
+	}
+	else {
+		return "F";
+	}
 }
+
